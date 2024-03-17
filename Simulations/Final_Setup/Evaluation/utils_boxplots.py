97ff72158b40d1198a4a1d010e2b31ba6ed09ff1
nadjasauter <nadja.sauter@campus.lmu.de>
2024-03-17 19:16:59 +0100

    writing results part report

--- Simulations/Final_Setup/Evaluation/utils_boxplots.py
+++ Simulations/Final_Setup/Evaluation/utils_boxplots.py
@@ -27,7 +27,7 @@ def flatten_data(data_all):
 
 
 
-def plot_eval(value_vars , value_name, data, model_vars_title, transformation = 'None', model = 'None', figsize = (6, 3)):
+def plot_eval(value_vars , value_name, data, model_vars_title, transformation = 'None', model = 'None', figsize = (6, 4)):
         '''
         value_vars: list of strings, names of columns to be plotted for stratified and unstratified ['error_estimator_mean_stratified', 'error_estimator_mean_unstratified']. 
                         Important: stratified and unstratified must be written in the end after '_' otherwise automatic labeling won't work and you'll get an error
@@ -35,6 +35,7 @@ def plot_eval(value_vars , value_name, data, model_vars_title, transformation = 
         value_name: string, name of the value variable to be plotted. Basically name of value_vars that is plotted on the y-axis
         differences_table_all: pd.DataFrame, table of differences in mean and sd of stratified and unstratified , output of function with plots
         '''
+        plots_report_path = '/Users/nadja/Documents/University/Master_Statistik/WS2023_24/SLDS/slds_stratification/Simulations/Final_Setup/Evaluation/plots/report_plots'
         differences_table_all = pd.DataFrame()
         # make data long
         data_long = data.melt(id_vars=['param_model'], 
@@ -65,29 +66,37 @@ def plot_eval(value_vars , value_name, data, model_vars_title, transformation = 
         ### Plot
         plt.figure(figsize= figsize)  # Set the figure size to 10 inches by 6 inches
         if model == 'None' and transformation != 'None':
-                plt.title('Transformation ' + transformation, fontsize=14 )
+                plt.title('Transformation ' + transformation, fontsize=15)
         elif transformation == 'None' and model != 'None':
-                plt.title('Model: ' + model, fontsize=14)
+                plt.title('Model: ' + model, fontsize=15)
         elif transformation != 'None' and model != 'None':
-                plt.title('Transformation: ' + transformation, fontsize=14)
+                plt.title('Transformation: ' + transformation, fontsize=15)
         else:
-                plt.title('Ordered and grouped boxplot', fontsize=14)
+                plt.title('Ordered and grouped boxplot', fontsize=15)
         sns.boxplot(x= data_long['param_model'], 
                         y= data_long[value_name], 
                         hue= data_long['Stratification'],  palette={value_vars[0]: 'royalblue', 
                         value_vars[1]: 'orangered'}, 
                         showfliers=False, # hide outliers
                         order = list(mean_intersection['param_model']))  #showmeans=True, meanline=True
-        plt.yticks(fontsize=12)  
-        plt.xticks(rotation=90, fontsize=12)  # rotate x labels by 90 degrees
-        plt.ylabel(value_name, fontsize=12)
-        plt.xlabel('Experimental Parameter Combination: '+ model_vars_title,  fontsize=12)
-        legend = plt.legend( fontsize=12)
+        plt.yticks(fontsize=14)  
+        plt.xticks(rotation=90, fontsize=14)  # rotate x labels by 90 degrees
+        plt.ylabel(value_name, fontsize=14)
+        plt.xlabel('Experimental Parameter Combination: '+ model_vars_title,  fontsize=14)
+        legend = plt.legend( fontsize=14)
         for i, label in enumerate(legend.get_texts()):
                 if label.get_text() == value_vars[0]:
                         label.set_text('Stratified')
                 elif label.get_text() == value_vars[1]:
-                        label.set_text('Unstratified')
+                        label.set_text('Unstratified')#
+        if transformation == 'None' and model == 'None':
+                plt.savefig(plots_report_path + '/boxplot_' + value_name + "_all" + '.png', bbox_inches='tight')
+        elif transformation != 'None' and model == 'None':
+                plt.savefig(plots_report_path + '/boxplot_' + value_name + "_" + transformation + '.png', bbox_inches='tight')
+        elif transformation == 'None' and model != 'None':
+                plt.savefig(plots_report_path + '/boxplot_' + value_name + "_" + model + '.png', bbox_inches='tight')
+        elif transformation != 'None' and model != 'None':
+               plt.savefig(plots_report_path + '/boxplot_' + value_name + "_" + transformation +  "_"+ model + '.png', bbox_inches='tight')
         plt.show()
         differences.sort_values(by='Difference Mean', ascending=False, inplace=True)
         print(differences)
